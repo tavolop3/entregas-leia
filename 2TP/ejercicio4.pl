a08dacc3f26ffb7692cb2aad8d0e40f980e68009
tavolop3 <tavo.lop33@gmail.com>
2024-12-07 15:11:54 -0300

    corrijo el 4

--- 2TP/ejercicio4.pl
+++ 2TP/ejercicio4.pl
@@ -1,8 +1,8 @@
-es_bueno(Alguien) :- hace(Alguien, Algo), bueno(Algo).
-es_malo(Alguien) :- hace(Alguien, Algo), malo(Algo).
+bueno(ayudar_madre).
+malo(mentir).
 
 hace(sebastian, ayudar_madre).
 hace(sebastian, mentir).
 
-bueno(ayudar_madre).
-malo(mentir).
+es_bueno(Alguien) :- hace(Alguien, Algo), bueno(Algo).
+es_malo(Alguien) :- hace(Alguien, Algo), malo(Algo).
